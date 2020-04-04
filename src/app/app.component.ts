87abbbddeef64188e47f5fd221bfa91e1333ae91
Alessio Sferro <sferro.alessio@gmail.com>
2020-04-04 13:07:42 +0200

    Fix image url path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,36 @@
-import { IngredientsService } from './services/ingredients.service';
-import { Component, OnInit } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { Ingredient } from './models/ingredient';
+import { IngredientsService } from "./services/ingredients.service";
+import { Component, OnInit } from "@angular/core";
+import { ReplaySubject, Observable } from "rxjs";
+import { Ingredient } from "./models/ingredient";
+import { share, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: [`
-    .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-top: 100px;
-    }
-  `]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 100px;
+      }
+    `
+  ]
 })
 export class AppComponent implements OnInit {
   public ingredient$ = new ReplaySubject<Ingredient>(1);
+  private ingredients$: Observable<Ingredient[]>;
 
-  constructor(
-    private Ingredients: IngredientsService
-  ) { }
+  constructor(private Ingredients: IngredientsService) {}
 
   ngOnInit() {
-    this.getAnotherIngredient();
+    this.ingredients$ = this.Ingredients.getIngredients().pipe(shareReplay(1));
+    this.getIngredient();
   }
 
-  getAnotherIngredient() {
-    const ingredient$ = this.Ingredients.getRandomIngredient();
+  getIngredient() {
+    const ingredient$ = this.Ingredients.getRandomIngredient(this.ingredients$);
     ingredient$.subscribe(ingredient => this.ingredient$.next(ingredient));
   }
 }
--- src/app/services/ingredients.service.ts
+++ src/app/services/ingredients.service.ts
@@ -1,32 +1,31 @@
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
-import { environment } from './../../environments/environment';
+import { Observable } from "rxjs";
+import { map, take } from "rxjs/operators";
+import { environment } from "./../../environments/environment";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Ingredient } from '../models/ingredient';
+import { Ingredient } from "../models/ingredient";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class IngredientsService {
   readonly apiUrl: string = environment.apiUrl;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getIngredients(): Observable<Ingredient[]> {
     return this.http.get<Ingredient[]>(`${this.apiUrl}/ingredients`);
   }
 
-  getRandomIngredient(): Observable<Ingredient> {
-    return this.getIngredients()
-      .pipe(
-        take(1),
-        map(ingredients => {
-          const index = Math.floor((Math.random() * ingredients.length));
-          return ingredients[index];
-        }),
-      )
+  getRandomIngredient(
+    ingredients$: Observable<Ingredient[]>
+  ): Observable<Ingredient> {
+    return ingredients$.pipe(
+      take(1),
+      map(ingredients => {
+        const index = Math.floor(Math.random() * ingredients.length);
+        return ingredients[index];
+      })
+    );
   }
 }
